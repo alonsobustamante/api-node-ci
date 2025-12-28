517601dcbba32b832989c73f0a19d9549ac64132
Alonso Bustamante <proveedorbigcheese14@culqi.com>
2025-12-28 07:12:54 -0500

    feat: add test for version

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    testEnvironment: 'node',
+    collectCoverageFrom: ['src/**/*.js'],
+    coverageThreshold: {
+        global: {
+        branches: 70,
+        functions: 70,
+        lines: 70,
+        statements: 70
+        }
+    }
+};
--- tests/app.test.js
+++ tests/app.test.js
@@ -3,8 +3,14 @@ const app = require("../src/app")
 
 describe("GET /health", () => {
     it("should return status ok", async () => {
-        const res = await request(app).get("/health");
-        expect(res.statusCode).toBe(200);
-        expect(res.body).toEqual({status: "ok"});
+      const res = await request(app).get("/health");
+      expect(res.statusCode).toBe(200);
+      expect(res.body).toEqual({status: "ok"});
+    });
+
+    it('GET /version should return version', async () => {
+      const res = await request(app).get('/version');
+      expect(res.statusCode).toBe(200);
+      expect(res.body).toEqual({ version: '1.0.0' });
     });
 });
