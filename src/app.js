03c348410c84485273c3c4d6e03d0c7081b45100
Alonso Bustamante <proveedorbigcheese14@culqi.com>
2025-12-28 06:44:20 -0500

    feat: add version endpoint

--- src/app.js
+++ src/app.js
@@ -5,4 +5,9 @@ app.get("/health", (req, res) => {
     res.status(200).json({status: "ok"})
 })
 
+app.get("/version", (req, res) => {
+  res.status(200).json({ version: '1.0.0' });
+});
+
+
 module.exports = app;
